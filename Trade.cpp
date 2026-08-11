96ae18c5c9bc122912d17b024e61fb4c6c74cbb7
Damien <damienchimsengyuan@gmail.com>
2026-08-11 10:28:38 +1000

    Added trade class

---
+++ Trade.cpp
@@ -0,0 +1,27 @@
+#include "Trade.hpp"
+#include <string>
+
+Trade::Trade(const std::string& trade_id, const std::string& stock_id, const std::string& buyer_id, const std::string& seller_id, int quantity, int price) :
+    trade_id{ trade_id }, stock_id{ stock_id }, buyer_id{ buyer_id }, seller_id{ seller_id }, quantity{ quantity }, price{ price } {
+
+}
+
+const std::string& Trade::get_trade_id() {
+    return trade_id;
+}
+
+const std::string& Trade::get_stock_id() {
+    return stock_id;
+}
+const std::string& Trade::get_buyer_id() {
+    return buyer_id;
+}
+const std::string& Trade::get_seller_id() {
+    return seller_id;
+}
+int Trade::get_quantity() {
+    return quantity;
+}
+int Trade::get_price() {
+    return price;
+}
---
+++ Trade.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <string>
+class Trade {
+private:
+    std::string trade_id;
+    std::string stock_id;
+    std::string buyer_id;
+    std::string seller_id;
+
+    int quantity;
+    int price;
+public:
+    Trade(const std::string& trade_id, const std::string& stock_id, const std::string& buyer_id, const std::string& seller_id, int quantity, int price);
+    const std::string& get_trade_id();
+    const std::string& get_stock_id();
+    const std::string& get_buyer_id();
+    const std::string& get_seller_id();
+    int get_quantity();
+    int get_price();
+};
