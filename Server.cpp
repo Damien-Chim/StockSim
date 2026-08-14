530400f9d863a16fb44f3d4aee5542c29504ce73
Damien <damienchimsengyuan@gmail.com>
2026-08-14 22:31:53 +1000

    Basic register user

--- Server.cpp
+++ Server.cpp
@@ -1,13 +1,63 @@
 #include "Server.hpp"
+#include "Exchange.hpp"
 #include <boost/asio.hpp>
 #include <iostream>
 #include <thread>
 #include <vector>
 #include <sstream>
+#include <string>
+
 using boost::asio::ip::tcp;
 
 Server::Server(int port) : port{ port } {
 
+}
+
+static std::vector<std::string> split(std::string s, const std::string& delimiter) {
+    std::vector<std::string> tokens;
+    size_t pos = 0;
+    std::string token;
+    while ((pos = s.find(delimiter)) != std::string::npos) {
+        token = s.substr(0, pos);
+        tokens.push_back(token);
+        s.erase(0, pos + delimiter.length());
+    }
+    tokens.push_back(s);
+
+    return tokens;
+}
+
+static std::string process_command(std::string request) {
+    std::vector<std::string> tokens = split(request, " ");
+    if (tokens.empty()) {
+        return "Invalid Command\n";
+    }
+
+    std::string instruction = tokens[0];
+    if (instruction == "register") {
+        if (tokens.size() != 2) { return "Invalid Command"; }
+        std::string username = tokens[1];
+        Exchange::add_user(User(username));
+        return "Successfully registered user\n";
+    }
+
+    else if (instruction == "get_id") {
+
+    }
+
+    else if (instruction == "get_available_cash") {
+
+    }
+
+    else if (instruction == "get_reserved_cash") {
+
+    }
+    
+    else if (instruction == "get_available_cash") {
+
+    }
+
+    return "Invalid Command\n";
 }
 
 static void client_thread(tcp::socket socket) {
@@ -20,8 +70,8 @@ static void client_thread(tcp::socket socket) {
             std::istream input(&buffer);
             std::string line;
             std::getline(input, line);
-
-            std::string response = "Pong\n";
+            
+            std::string response = process_command(line);
             boost::asio::write(socket, boost::asio::buffer(response));
         }
     }
