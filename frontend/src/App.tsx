754cd4e464dfb6c8211a1b387ddd2b1e180d4fa8
Damien <damienchimsengyuan@gmail.com>
2026-08-17 20:19:00 +1000

    basic frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import CandlestickChart from "./CandlestickChart";
+
+function App() {
+    return (
+        <div>
+            <h1>StockSim</h1>
+            <CandlestickChart />
+        </div>
+    );
+}
+
+export default App;
